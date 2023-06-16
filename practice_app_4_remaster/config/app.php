6966917be6872eb004cbac79df82c60d96b26229
dungnc <dungnc@deha-soft.com>
2023-06-16 21:18:51 +0700

    add MaterialPresetServiceProvider::class to config/app.php

--- practice_app_4_remaster/config/app.php
+++ practice_app_4_remaster/config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        LaravelFrontendPresets\MaterialPreset\MaterialPresetServiceProvider::class,
         /*
          * Application Service Providers...
          */
