01d337b891136ba02c080888a93f60c3be5cbd5a
dungnc <dungnc@deha-soft.com>
2023-06-21 09:07:37 +0700

    re-add side-bar toggle

--- practice_app_4_remaster/resources/views/components/navbars/navs/auth.blade.php
+++ practice_app_4_remaster/resources/views/components/navbars/navs/auth.blade.php
@@ -27,6 +27,16 @@
                             Out</span>
                     </a>
                 </li>
+                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                        </div>
+                    </a>
+                </li>
+
             </ul>
         </div>
     </div>
