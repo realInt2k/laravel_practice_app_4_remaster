b90c4378eb0da46a821dfb594bb52c785df0d0a7
dungnc <dungnc@deha-soft.com>
2023-06-23 11:16:22 +0700

    correctly represent 5mb

--- practice_app_4_remaster/app/Http/Requests/UpdateProductRequest.php
+++ practice_app_4_remaster/app/Http/Requests/UpdateProductRequest.php
@@ -31,7 +31,7 @@ class UpdateProductRequest extends FormRequest
         return [
             'name' => 'required',
             'description' => 'required',
-            'image' => 'image|mimes:jpeg,png,jpg|max:5000',
+            'image' => 'image|mimes:jpeg,png,jpg|max:5120',
         ];
     }
 }
