ec5f2715fcd536b5729cdd11ac96a40d7f8d17b1
dungnc <dungnc@deha-soft.com>
2023-06-25 13:27:05 +0700

    UserController add typings

--- practice_app_4_remaster/app/Http/Controllers/UserController.php
+++ practice_app_4_remaster/app/Http/Controllers/UserController.php
@@ -2,39 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateProfileRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Services\PermissionService;
+use App\Services\ProductService;
 use App\Services\RoleService;
 use App\Services\UserService;
-use Illuminate\Http\Response;
-use App\Services\PermissionService;
-use App\Http\Requests\StoreUserRequest;
-use App\Http\Requests\UpdateUserRequest;
-use App\Http\Requests\UpdateProfileRequest;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application as FoundationApplication;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public PermissionService $permissionService;
     public RoleService $roleService;
     public UserService $userService;
+    public ProductService $productService;
 
     public function __construct(
-        UserService $userService,
-        RoleService $roleService,
-        PermissionService $permissionService
-    ) {
+        UserService       $userService,
+        RoleService       $roleService,
+        PermissionService $permissionService,
+        ProductService    $productService
+    )
+    {
         $this->userService = $userService;
         $this->roleService = $roleService;
         $this->permissionService = $permissionService;
+        $this->productService = $productService;
     }
 
-    public function index(Request $request)
+    public function index(): View|FoundationApplication|Factory|Application
     {
         $roles = $this->roleService->getAllRoles();
         return view('pages.users.index', compact('roles'));
     }
 
-    public function search(Request $request)
+    public function search(Request $request): JsonResponse
     {
         $users = $this->userService->search($request, self::PER_PAGE);
         $roles = $this->roleService->getAllRoles();
@@ -46,14 +56,14 @@ class UserController extends Controller
         return $this->responseJSON($viewHtml);
     }
 
-    public function show(Request $request, $id)
+    public function show(int $id): JsonResponse
     {
         $user = $this->userService->getById($id);
         $viewHtml = view('pages.users.show', compact('user'))->render();
         return $this->responseJSON($viewHtml);
     }
 
-    public function create()
+    public function create(): JsonResponse
     {
         $roles = $this->roleService->getAllRoles();
         $permissions = $this->permissionService->getAllPermissions();
@@ -61,53 +71,71 @@ class UserController extends Controller
         return $this->responseJSON($viewHtml);
     }
 
-    public function store(StoreUserRequest $request)
+    public function store(StoreUserRequest $request): JsonResponse
     {
         $user = $this->userService->store($request);
         return $this->responseJSON($user);
     }
 
-    public function edit(Request $request, $id)
+    public function edit(int $id): JsonResponse
     {
         $user = $this->userService->getById($id);
         $roles = $this->roleService->getAllRoles();
         $permissions = $this->permissionService->getAllPermissions();
-
         $viewHtml = view('pages.users.edit', compact('user', 'roles', 'permissions'))->render();
         return $this->responseJSON($viewHtml);
     }
 
-    public function updateProfile(UpdateProfileRequest $request)
+    /**
+     * @throws Exception
+     */
+    public function updateProfile(UpdateProfileRequest $request): JsonResponse
     {
-        $id = auth()->user()->id;
+        DB::beginTransaction();
         try {
+            $id = auth()->user()->id;
             $user = $this->userService->update($request, $id, true);
         } catch (Exception $e) {
-            return $this->responseWhenException($request, $e);
+            DB::rollBack();
+            $this->throwException('cannot update user through profile', $e);
         }
+        DB::commit();
         return $this->responseJSON($user);
     }
 
-    public function update(UpdateUserRequest $request, $id)
+    /**
+     * @throws Exception
+     */
+    public function update(UpdateUserRequest $request, int $id): JsonResponse
     {
+        DB::beginTransaction();
         try {
             $user = $this->userService->update($request, $id);
         } catch (Exception $e) {
-            return $this->responseWhenException($request, $e);
+            DB::rollBack();
+            $this->throwException('cannot update user', $e);
         }
+        DB::commit();
         return $this->responseJSON($user);
     }
 
-    public function destroy(Request $request, $id)
+    /**
+     * @throws Exception
+     */
+    public function destroy($id): JsonResponse
     {
         if (auth()->user()->id == $id) {
             auth()->logout();
         }
+        DB::beginTransaction();
         try {
             $user = $this->userService->destroy($id);
         } catch (Exception $e) {
-            return $this->responseWhenException($request, $e);
+            DB::rollBack();
+            $this->throwException('cannot destroy user', $e);
         }
+        DB::commit();
+        $this->productService->unAttachUser($user->id);
         return $this->responseJSON($user);
     }
 }
