7e12f42f2bdeede518061f3bf63a980aaf1f8c76
int2k <dungnc@deha-soft.com>
2023-06-25 22:51:28 +0700

    Show CATEGORY TEST

--- practice_app_4_remaster/README.md
+++ practice_app_4_remaster/README.md
@@ -3,6 +3,6 @@
 - Go to `${host}/documentation/getting-started/installation.html` to see available syntax for css
 ## Eloquent
 - command `art ide-helper:models` helps you create phpDocs for syntax like eloquent:
-  - For example: `art ide-helper:models 'App\Models\Products'`
+  - For example: `art ide-helper:models 'App\Models\Product'`
   - Remember to change `@mixin \Eloquent` to `@mixin Builder` + clean up code.
   - Install Laravel-Query plugin.
--- practice_app_4_remaster/app/Models/Category.php
+++ practice_app_4_remaster/app/Models/Category.php
@@ -2,55 +2,72 @@
 
 namespace App\Models;
 
+use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $name
+ * @property int|null $parent_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Category> $children
+ * @property-read int|null $children_count
+ * @property-read Category|null $parent
+ * @property-read Collection<int, Product> $products
+ * @property-read int|null $products_count
+ * @method static CategoryFactory factory($count = null, $state = [])
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category ofUserId(int $id)
+ * @method static Builder|Category query()
+ * @method static Builder|Category whereCreatedAt($value)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereName($value)
+ * @method static Builder|Category whereParentId($value)
+ * @method static Builder|Category whereUpdatedAt($value)
+ * @mixin Builder
+ */
 class Category extends Model
 {
     use HasFactory;
+
     protected $table = 'categories';
     protected $fillable = [
         'name',
         'parent_id'
     ];
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function products()
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'category_product', 'category_id', 'product_id');
     }
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function children()
+    public function children(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id');
     }
 
-    /**
-     * @return Illuminate\Database\Eloquent\Builder|null
-     */
-    public function scopeOfUserId(Builder $query, int $id)
+    public function scopeOfUserId(Builder $query, int $id): ?Builder
     {
         return $query->wherein("id", Product::onlyUserId($id)->pluck('category_id')->toArray());
     }
 
-    /**
-     * @return Illuminate\Database\Eloquent\Builder|null
-     */
-    public function scopeWhereName(Builder $query, string|null $name)
+    public function scopeWhereName(Builder $query, string|null $name): ?Builder
     {
         return $name != null ? $query->where('name', 'like', '%' . $name . '%') : null;
     }
