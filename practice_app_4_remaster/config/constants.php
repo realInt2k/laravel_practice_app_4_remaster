8fd132e09ef13363a0c6e97ddc97cba93b09c74a
int2k <dungnc@deha-soft.com>
2023-06-26 10:47:54 +0700

    rename routes

--- practice_app_4_remaster/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'AUTHENTICATION_ERROR_KEY' => 'authenticationErrors',
-    'FRONT_END_DEBUG' => true,
-    'SUPER_ADMIN_ROLE' => 'super-admin',
-    'ADMIN_ROLE' => 'admin'
-];
--- practice_app_4_remaster/routes/web.php
+++ practice_app_4_remaster/routes/web.php
@@ -15,18 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 /*
- * Backend Routes
+ * Models Routes
  *
  * These routes can only be accessed by authenticated users
  */
 
 Route::group(['middleware' => 'auth'], function () {
-    includeRouteFiles(__DIR__ . '/backend/');
+    includeRouteFiles(__DIR__ . '/models/');
 });
 
 /*
- * Frontend Routes
+ * Sign-in Sign-up Routes
  *
  * These routes validates users, or let them signup
  */
-includeRouteFiles(__DIR__ . '/frontend/');
+includeRouteFiles(__DIR__ . '/authentication/');
