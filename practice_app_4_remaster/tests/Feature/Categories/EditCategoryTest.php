81c2ddab4f7a3489d699260dd32e9e2023a6fa87
int2k <dungnc@deha-soft.com>
2023-06-26 09:00:13 +0700

    edit & create category test fix

--- practice_app_4_remaster/tests/Feature/Categories/EditCategoryTest.php
+++ practice_app_4_remaster/tests/Feature/Categories/EditCategoryTest.php
@@ -8,51 +8,55 @@ use Illuminate\Testing\Fluent\AssertableJson;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\Feature\TestCaseUtils;
 
-class CreateCategoryTest extends TestCaseUtils
+class EditCategoryTest extends TestCaseUtils
 {
     /** @test */
-    public function can_not_see_create_category_form_if_unauthenticated(): void
+    public function can_not_see_edit_category_form_if_unauthenticated(): void
     {
-        $response = $this->get(route('categories.create'));
+        $category = Category::factory()->create();
+        $response = $this->get($this->getRoute($category->id));
         $response->assertStatus(Response::HTTP_FOUND)
             ->assertRedirect(route('login'));
     }
 
     /** @test */
-    public function authenticated_cannot_see_create_category_form_without_appropriate_permission(): void
+    public function authenticated_cannot_see_edit_category_form_without_appropriate_permission(): void
     {
         $this->loginAsNewUser();
-        $response = $this->get(route('categories.create'));
+        $category = Category::factory()->create();
+        $response = $this->get($this->getRoute($category->id));
         $response->assertStatus(Response::HTTP_FOUND)
             ->assertSessionHasErrors($this->getAuthErrorKey());
     }
 
     /** @test */
-    public function admin_cannot_see_create_category_form_without_appropriate_permission(): void
+    public function admin_cannot_see_edit_category_form_without_appropriate_permission(): void
     {
         $this->loginAsNewUserWithRole($this->getAdminRole());
-        $response = $this->get(route('categories.create'));
+        $category = Category::factory()->create();
+        $response = $this->get($this->getRoute($category->id));
         $response->assertStatus(Response::HTTP_FOUND)
             ->assertSessionHasErrors($this->getAuthErrorKey());
     }
 
     /** @test */
-    public function admin_can_see_create_category_form_with_appropriate_permission(): void
+    public function admin_can_see_edit_category_form_with_appropriate_permission(): void
     {
-        $this->loginAsNewUserWithRoleAndPermission($this->getAdminRole(), 'categories.store');
+        $this->loginAsNewUserWithRoleAndPermission($this->getAdminRole(), 'categories.update');
         $this->try_to_get_category_create_form_with_appropriate_permission();
     }
 
     /** @test */
-    public function non_admin_can_see_create_category_form_with_appropriate_permission(): void
+    public function non_admin_can_see_edit_category_form_with_appropriate_permission(): void
     {
-        $this->loginAsNewUserWithRoleAndPermission('role' . Str::random(5), 'categories.store');
+        $this->loginAsNewUserWithRoleAndPermission('role' . Str::random(5), 'categories.update');
         $this->try_to_get_category_create_form_with_appropriate_permission();
     }
 
     public function try_to_get_category_create_form_with_appropriate_permission(): void
     {
-        $response = $this->get(route('categories.create'));
+        $category = Category::factory()->create();
+        $response = $this->get($this->getRoute($category->id));
         $response->assertStatus(Response::HTTP_OK);
         $response
             ->assertStatus(Response::HTTP_OK)
@@ -60,6 +64,12 @@ class CreateCategoryTest extends TestCaseUtils
                 fn(AssertableJson $json) => $json
                     ->where('data', fn ($data) => !empty($data))
                     ->etc()
-            );
+            )
+            ->assertSee($category->name);
+    }
+
+    public function getRoute(int $id): string
+    {
+        return route('categories.edit', $id);
     }
 }
